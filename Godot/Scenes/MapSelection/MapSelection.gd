b0e268b65c14ea27e3789cbce942383c93c96cf7
pappkasse <pappkasse2121@gmail.com>
2021-09-01 19:08:10 +0200

    Add selection box

---
+++ Godot/Scenes/MapSelection/MapSelection.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+export (float) var speed: float = 15;
+
+func _input(_event):
+	var ray_length = 1000;
+	var mouse_pos = get_viewport().get_mouse_position();
+	var camera = get_viewport().get_camera();
+	var from = camera.global_transform.origin;
+	var to = from + camera.project_ray_normal(mouse_pos) * ray_length;
+	
+	var space_state = get_world().get_direct_space_state();
+	var result = space_state.intersect_ray(from, to, [], 0b1);
+	
+	# Move selection box to colliding object using tween
+	if result:
+		$Tween.interpolate_property(self, "global_transform:origin", 
+		null, result.collider.global_transform.origin, 1 / speed, 
+		Tween.TRANS_LINEAR, Tween.EASE_IN);
+		$Tween.start();
