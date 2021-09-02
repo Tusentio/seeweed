f2ba8f80a38f7ccd87e8ed85264cdb2f8231978c
pappkasse <pappkasse2121@gmail.com>
2021-09-02 19:58:15 +0200

    Fix player animations

--- Godot/Scenes/Player/Player.gd
+++ Godot/Scenes/Player/Player.gd
@@ -10,8 +10,18 @@ func _physics_process(delta):
 	velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left");
 	velocity.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward");
 	
-	if velocity != Vector3.ZERO:
+	if velocity.x || velocity.z:
+		# Reset animation
+		if ($Animator.current_animation == "idle"):
+			$Animator.seek(0, true);
+			
+		$Animator.play("walk");
 		$Body.look_at(translation + velocity, Vector3.UP);
+	else:
+		# Reset animation and play idle
+		if ($Animator.current_animation == "walk"):
+			$Animator.seek(0, true);
+			$Animator.play("idle");
 	
 	velocity.y -= gravity * delta;
 	
