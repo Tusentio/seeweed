46604c47737330fe30070076486e62b26389cca0
Octaphous <pappkasse2121@gmail.com>
2021-09-07 20:59:47 +0200

    Stack count text now working

--- Godot/Scenes/HUD/Slot/Slot.gd
+++ Godot/Scenes/HUD/Slot/Slot.gd
@@ -13,8 +13,15 @@ func bind(inv: Inventory, slot: int = get_index()):
 # When any inventory slot gets updated
 func _on_inventory_update(slot, _delta):
 	if slot == bound_slot:
+		# Set slot image
 		var item = inventory.get_item_at(bound_slot);
 		$ItemPreview.set_texture(item.icon if item else null);
+		
+		# Set count label text and visibility
+		var count = inventory.get_count_at(bound_slot);
+		$StackCount.visible = count != 1;
+		$StackCount.text = String(count);
+		
 		wobble();
 	
 # Play wobble animation on slot rect
