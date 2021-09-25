4ede6331276cf8b9c31fe19e89976e902557c6a9
Keve1227 <killenlillen02@gmail.com>
2021-09-25 22:19:15 +0200

    Fix map selection

--- Godot/Scenes/MapSelection/MapSelection.gd
+++ Godot/Scenes/MapSelection/MapSelection.gd
@@ -1,40 +1,42 @@
 extends Spatial
 
 export (float) var speed: float = 25;
-export (float) var max_distance: float = 2;
+export (float) var max_player_distance: float = 3;
 onready var player: Player = get_tree().get_nodes_in_group("Player")[0];
 
-const origin_offset = Vector3(0.5, 0.5, 0.5);
+const RAY_LENGTH := 1000;
+const EPSILON := 0.01;
 
 func _physics_process(_delta):
-	# Creat ray
-	var ray_length = 1000;
-	var mouse_pos = get_viewport().get_mouse_position();
-	var camera = get_viewport().get_camera();
-	var from = camera.global_transform.origin;
-	var to = from + camera.project_ray_normal(mouse_pos) * ray_length;
+	# Create ray
+	var mouse_pos := get_viewport().get_mouse_position();
+	var camera := get_viewport().get_camera();
+	var from := camera.global_transform.origin;
+	var to := from + camera.project_ray_normal(mouse_pos) * RAY_LENGTH;
+	var ray_normal := (to - from).normalized();
 	
-	var space_state = get_world().get_direct_space_state();
-	var result = space_state.intersect_ray(from, to, [], 0b1);
+	var space_state := get_world().get_direct_space_state();
+	var result := space_state.intersect_ray(from, to, [], 0b0001);
 	
 	# If ray didn't hit anything
 	if not result:
 		visible = false;
 		return;
 	
-	# Get hit object position
-	var result_pos = result.collider.global_transform.origin;
+	var position: Vector3 = result.position + ray_normal * EPSILON;
+	var tile_position := position.floor();
+	var center_position = tile_position + Vector3.ONE / 2;
 	
-	# If object and player are too far apart
-	if (result_pos + origin_offset).distance_to(player.global_transform.origin) > max_distance:
+	# If hit position and player are too far apart
+	if center_position.distance_to(player.global_transform.origin) > max_player_distance:
 		visible = false;
 		return;
 	
-	# Move selection box to colliding object using tween
-	if global_transform.origin != result_pos or !visible:
+	# Move selection box to colliding tile and reset animation
+	if global_transform.origin != tile_position:
 		$Animator.seek(0, true);
-		visible = true;
-		global_transform.origin = result_pos;
+		global_transform.origin = tile_position;
+	visible = true;
 
 # Get cursor position
 func pos() -> Vector3:
