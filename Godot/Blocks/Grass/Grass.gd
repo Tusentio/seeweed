c362f1e87d802b95dc62844933ca9d0e87b0a58a
Keve1227 <killenlillen02@gmail.com>
2021-10-10 18:53:32 +0200

    Standardize random block orientations

--- Godot/Blocks/Grass/Grass.gd
+++
@@ -1,6 +0,0 @@
-extends Block
-
-const random_orientations := [0, 10];
-
-func get_orientation(position: Vector3, terrain: Object, random: RandomNumberGenerator) -> int:
-	return random_orientations[random.randi_range(0, random_orientations.size() - 1)];
